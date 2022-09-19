03eb88ef92b377089a6e749af397fe4ead9d4ad9
ewpac <ew.pach@gmail.com>
2022-09-19 21:06:22 +0200

    added new class Main

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Wprowadź wartość x");
+        int x = scanner.nextInt();
+        if(x<3){
+            System.out.println("x jest <3");
+        }else {
+            System.out.println("x jest >3");
+        }
+
+
+
+            }
+}
