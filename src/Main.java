acff9dccef7a7e3dc96501e3166b3eddc06f8221
ewpac <ew.pach@gmail.com>
2022-09-20 18:54:39 +0200

    Nowe zadanie domowe

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,18 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Wprowadź wartość x");
-        int x = scanner.nextInt();
-        if(x<3){
-            System.out.println("x jest <3");
+        System.out.println("Hello! what's your name?");
+        String name = scanner.nextLine();
+        System.out.println("Please enter your last name.");
+        String lastName = scanner.nextLine();
+        System.out.println("How old are you?");
+        int age = scanner.nextInt();
+        if(age<21) {
+            System.out.println(name + " " + lastName + ", you are too young to come in.");
         }else {
-            System.out.println("x jest >3");
+            System.out.println(name + " " + lastName + ", you can come in.");
         }
 
 
